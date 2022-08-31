1f3fd211d012f37c1ac8bd3e643ab0387fb80683
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-08-31 15:26:03 +0300

    Liking Likes

---
+++ WEB FUNDAMENTALS/Java-Script/Liking Likes/Liking Likes.js
@@ -0,0 +1,20 @@
+var like1 = document.querySelector(".L1");
+var like2 = document.querySelector(".L2");
+var like3 = document.querySelector(".L3");
+var counter1 = 9;
+
+
+function addLike1(){
+    counter1 ++;
+    like1.innerText = counter1 + " like(s)";
+}
+var counter2 = 12;
+function addLike2(){
+    counter2 ++;
+    like2.innerText = counter2 + " like(s)";
+}
+var counter3 = 9;
+function addLike3(){
+    counter3 ++;
+    like3.innerText = counter3 + " like(s)";
+}
