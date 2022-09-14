d744519081eb8e87457e0c0085d985cb6b4fd41e
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-09-14 11:37:19 +0300

    Users with Bank Accounts

--- python_stack/my_environments/py3Env/Users with Bank Accounts.py
+++ python_stack/my_environments/py3Env/Users with Bank Accounts.py
@@ -20,7 +20,6 @@ class User:
     def __init__(self, name, email, intRate, amount):
         self.name = name
         self.email = email
-        self.intRate = intRate
         self.account_balance = BankAccount(intRate= intRate, balance= amount)
     
     def make_deposit(self, amount):
@@ -33,9 +32,8 @@ class User:
         print("User:", self.name,", Balance: $", self.account_balance.account_balance)
     
     def interest(self):
-        self.account_balance.account_balance =self.account_balance.account_balance * self.intRate + self.account_balance.account_balance
-        
-        
+        self.account_balance.yield_interest()
+
 haneen = User("haneen", "haneen@python.com",0.03 , 700)
 ahmad = User("ahmad", "ahmad@python.com",0.2 , 500)
 thabet = User("thabet", "thabet@python.com",0.3 , 300)
@@ -44,4 +42,3 @@ haneen.make_deposit(100)
 haneen.make_withdrawal(50)
 haneen.interest()
 haneen.display_user_balance()
-
