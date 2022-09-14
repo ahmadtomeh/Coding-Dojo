9849d60bf894154dacad4040c35708fd05089b17
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-09-15 00:14:54 +0300

    Functions Intermediate I

---
+++ python_stack/my_environments/py3Env/Functions Intermediate I.py
@@ -0,0 +1,16 @@
+import random
+def randInt(min = '' , max = ''):
+    num = random.random()
+    if min == '' and max == '':
+        num = int(random.random() * 100)
+    if min == '' and max != '':
+        num = int(random.random() * 50)
+    if min != '' and max == '':
+        num = int(random.random() * (100 - min) + min)
+    if min != '' and max != '':
+        num = int(random.random() * (max - min) + min)
+    return num
+print("random integer between 0 to 100 >>>>>>  ", "[", randInt() , "]")
+print("random integer between 0 to 50 >>>>>>>  ", "[", randInt(max=50) , "]")
+print("random integer between 50 to 100 >>>>>  ", "[", randInt(min=50) , "]")
+print("random integer between 50 and 500 >>>>  ", "[", randInt(min=50, max=500) , "]")
