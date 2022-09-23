b286d0e18917b11c5509bbd1c9b602302e23946b
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-09-23 19:46:15 +0300

    Great_Number_Game

--- python_stack/django/django_intro/Great_Number_Game/app_one/urls.py
+++ python_stack/django/django_intro/Great_Number_Game/app_one/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('result', views.input)
+    path('result', views.input),
+    path('reset', views.reset)
 ]
--- python_stack/django/django_intro/Great_Number_Game/app_one/views.py
+++ python_stack/django/django_intro/Great_Number_Game/app_one/views.py
@@ -1,25 +1,34 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
 
-def index(request):
 
-    request.session ['rand'] = random.randint(1, 100)
-    
+def index(request):
+    request.session['x'] = 5
+    request.session['rand'] = random.randint(1, 100)
     return render(request, 'index.html')
-    
-def input (request):
-    p=''
+
+
+def input(request):
+    request.session['x'] -= 1
     input = int(request.POST['input'])
+    p = ''
     if input == request.session['rand']:
-        p = 'correct'
-    elif input >  request.session['rand']:
+        p = ' Was The Number!'
+    elif input - 10 > request.session['rand']:
+        p = 'Too hight'
+    elif input + 10 < request.session['rand']:
+        p = 'Too low'
+    elif input > request.session['rand']:
         p = 'hight'
     elif input < request.session['rand']:
         p = 'low'
     context = {
         "p1": p,
-        "num":input
+        "input": input,
     }
     return render(request, 'index.html', context)
+
+
+def reset(request):
+    return redirect('/')
 # Create your views here.
