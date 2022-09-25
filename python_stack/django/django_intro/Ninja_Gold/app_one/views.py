3d35414878feacebe23c58023c7e578ac7615939
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-09-25 12:50:31 +0300

    Ninja Gold

---
+++ python_stack/django/django_intro/Ninja_Gold/app_one/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+import random
+from time import strftime
+
+def root(request):
+    if 'amount' in request.session:
+        pass
+    else:
+        request.session['amount']=0
+    return render (request, "index.html")
+
+def add(request):
+    time = strftime("%B %d, %Y %H:%M %p")
+    if request.POST['which']=='Quest':
+        gold= random.randint(-50, 50)
+        if gold < 0:    
+            if 'P' in request.session:
+                request.session ['P']+=[f"You failed a quest and lost {gold*-1} gold. Ouch.({time})"]
+        elif gold > 0:
+            if 'P' in request.session:
+                request.session ['P']+=[f"You completed a quest and earned {gold} gold.({time})"]
+    else:
+        gold = random.randint(10, 20)
+        if 'P' in request.session:
+                request.session ['P']+=[f"You entered a {request.POST['which']} and earned {gold} gold.({time})"]
+        
+    request.session.save()
+    request.session ['amount'] = request.session ['amount'] + gold
+    return redirect('/')
