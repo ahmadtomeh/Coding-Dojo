9fd74598804fe4b05bc19bcb6d06945b939c3dcb
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-09-26 16:20:54 +0300

    Users with Templates

--- python_stack/django/django_intro/user_shell/app_one/views.py
+++ python_stack/django/django_intro/user_shell/app_one/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# other imports
+from .models import User
+# show all of the data from a table
+def index(request):
+    context = {
+    	"all_the_users": User.objects.all()
+    }
+    return render(request, "UserswithTemplates.html", context)
+
+def add(request):
+    User.objects.create(first_name = request.POST['fname'], last_name = request.POST['lname'], email_address = request.POST['email'], age = request.POST['age'])
+    return redirect( '/')
+
+
 
 # Create your views here.
