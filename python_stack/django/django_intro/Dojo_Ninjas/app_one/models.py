84bb67fca838ed211d6e31afd963be7161755b87
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-09-26 21:26:57 +0300

    Dojo & Ninjas (Shell)

---
+++ python_stack/django/django_intro/Dojo_Ninjas/app_one/models.py
@@ -0,0 +1,20 @@
+from pydoc import describe
+from django.db import models
+
+class Dojo(models.Model):
+    name = models.CharField(max_length=55)
+    city = models.CharField(max_length=55)
+    state = models.CharField(max_length=55)
+    desc = models.CharField(max_length=55)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+class Ninja(models.Model):
+    dojo = models.ForeignKey(Dojo, related_name="books", on_delete = models.CASCADE)
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+# Create your models here.
---
+++ python_stack/django/django_intro/Dojo_Ninjas/app_one/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import renderz
+
+# Create your views here.
