a8bcf3e647664f3ae114a5f03d860fea14e45c3e
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-10-04 17:05:18 +0300

    Login & Registration

--- python_stack/django/django_fullstack/sRtS/app_1/models.py
+++ python_stack/django/django_fullstack/sRtS/app_1/models.py
@@ -7,13 +7,19 @@ class BlogManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
         # add keys and values to errors dictionary for each invalid field
-        if len(postData['title']) < 2:
+        if len(postData['title']) < 2 :
             errors["title"] = "show name should be at least 2 characters"
         x=postData['date']
-        y=(x[0:4])
-        y1=x[0]+x[1]+x[2]+x[3]
-        m=x[5]+x[6]
-        d=x[8]+x[9]
+        y1=0
+        m=0
+        d=0
+        if x:
+            y=(x[0:4])
+            y1=x[0]+x[1]+x[2]+x[3]
+            m=x[5]+x[6]
+            d=x[8]+x[9]
+        if not x:
+            errors["date"] = "The date must be entered in the date field"
         if  int(y1)> int(strftime ("%Y")):
             errors["date"] = "date description should be at least 10 characters"
         if int(m)> int(strftime ("%m")):
