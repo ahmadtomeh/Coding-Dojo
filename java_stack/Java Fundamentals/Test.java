f55d446334a5699a3b171a1a6a806f0dbe73e8eb
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-10-24 14:39:07 +0300

    First Java Program

---
+++ java_stack/Java Fundamentals/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("My name is Ahmad Tomeh");
+        System.out.println("I am 27 years old");
+        System.out.println("My hometown is Tulkarem, Palestine");
+    }
+}
