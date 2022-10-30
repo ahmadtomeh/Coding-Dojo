8cfd84ec02a7ef9ac075ac34824c0faf11ccb8b7
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-10-30 14:10:48 +0200

    Orders & Items

---
+++ java_stack/Java Fundamentals/OrdersAndItems/Order.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+public class Order {
+    private String name;
+    private double total;
+    private boolean ready;
+    private ArrayList<Item> items = new ArrayList<Item>();
+
+    public String getName() {
+        return name;
+    }
+
+    public Order() {
+        this("Guest");
+
+    }
+
+    public Order(String name) {
+        this.name = name;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public String statusMessage() {
+        if (ready) {
+            return "ready";
+        } else {
+            return "Not ready";
+        }
+    }
+
+    public void display() {
+        System.out.println("Customer Name: " + name);
+        Double total;
+        for (Item item : items) {
+            System.out.println(item.getName() + " : " + item.getPrice() + "$");
+        }
+        System.out.println("Total : " + getOrderTotal());
+        System.out.println(statusMessage());
+        System.out.println("___________________________");
+
+    }
+
+    public double getOrderTotal() {
+        double total = 0;
+        for (Item item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+}
---
+++ java_stack/Java Fundamentals/OrdersAndItems/TestOrders.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class TestOrders {
+    public static void main(String[] args) {
+        Item item1 = new Item("mocha", 3);
+        Item item2 = new Item("latte", 4);
+        Item item3 = new Item("drip coffee", 2);
+        Item item4 = new Item("cappuccino", 5);
+
+        Order order1 = new Order("Cindhuri");
+        Order order2 = new Order("Jimmy");
+        Order order3 = new Order("Noah");
+        Order order4 = new Order("sam");
+
+        order1.addItem(item1);
+        order1.addItem(item3);
+        order1.addItem(item2);
+        order1.addItem(item2);
+        order2.addItem(item2);
+        order2.addItem(item4);
+        order3.addItem(item1);
+        order3.addItem(item3);
+        order4.addItem(item4);
+        order4.addItem(item2);
+        order1.setReady(true);
+        order2.setReady(true);
+        order1.display();
+        order2.display();
+        order3.display();
+        order4.display();
+    }
+}
