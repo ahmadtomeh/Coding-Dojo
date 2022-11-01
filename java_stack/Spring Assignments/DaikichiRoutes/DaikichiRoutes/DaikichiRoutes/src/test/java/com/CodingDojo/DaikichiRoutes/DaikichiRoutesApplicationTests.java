eaf2e20dbf95034e9b02f3a3e31158a2603ba1b6
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-11-01 22:26:37 +0200

    Daikichi Routes

---
+++ java_stack/Spring Assignments/DaikichiRoutes/DaikichiRoutes/DaikichiRoutes/src/test/java/com/CodingDojo/DaikichiRoutes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodingDojo.DaikichiRoutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
