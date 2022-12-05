6542d58666d391f2c3bf8706a634d05176d7d9c7
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-12-05 13:32:02 +0200

    Hoisting

---
+++ MERN_stack/MERN Fundamentals/Hoisting.js
@@ -0,0 +1,103 @@
+console.log(hello);
+var hello = 'world';
+
+// OUTPUT : undefined
+
+//--------------------------------
+
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}   
+
+// OUTPUT : magnet
+
+//--------------------------------
+
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+
+// OUTPUT : super cool
+
+//--------------------------------
+
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+
+// OUTPUT : chicken >> half-chicken
+
+//--------------------------------
+
+mean();
+console.log(food);
+var mean = function() {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+// OUTPUT : ERROR
+
+//--------------------------------
+
+
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+// OUTPUT : undefined >> rock >> r&b >> disco
+
+//--------------------------------
+
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+// OUTPUT : san jose >> seattle >> burbank >> san jose
+
+//--------------------------------
+
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+
+// OUTPUT : { name: 'Chicago', students: 65, hiring: true } >> ERROR : dojo = "closed for now";
