9e1463c57d771134cebd974c7400159f627f14d5
Ahmad Tomeh <ahmad.r.tomeh@gmail.com>
2022-12-23 19:18:53 +0200

    Product Manager part 1

---
+++ MERN_stack/Full-Stack-MERN/HelloWorld/server/models/person.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const PersonSchema = new mongoose.Schema({
+    firstName: { type: String },
+    lastName: { type: String }
+}, { timestamps: true });
+
+const Person = mongoose.model('Person', PersonSchema);
+module.exports = Person;
